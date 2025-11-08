acc14cce9ea18ea98f9b16bf71746f307342e535
Suninwza <rujiphateingtham@gmail.com>
2025-11-08 17:58:25 +0700

    Player

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public int facingDirection = 1;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    // Update is called 50x per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+         
+        if(horizontal > 0 && transform.localScale.x < 0 ||
+           horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+     void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
+
+
